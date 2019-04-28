f1d04bd1baa54ff6cb3819b95b842556d7b66fb4
Saravanabalagi R <saravanabalagi@hotmail.com>
2019-04-28 13:56:08 +0100

    Adding tests

--- experiment.py
+++ experiment.py
@@ -5,34 +5,27 @@ class Experiment():
     def __init__(self, name='experiments', parent=None):
 
         if parent is not None and parent.path is not None:
-            self.path = os.path.join(parent.path, name)
-        else: self.path = os.path.join(name)
+            old_number = get_last_folder_number(parent.path)
+            new_number = old_number + 1
+
+            if len(name) > 0:
+                exp_folder = '{:02d}_{}'.format(new_number, name)
+            else:
+                exp_folder = '{:02d}'.format(new_number)
+            self.path = os.path.join(parent.path, exp_folder)
+        else:
+            self.path = os.path.join(name)
+
+        os.makedirs(self.path, exist_ok=True)
 
         self.name = name
         self.parent = parent
-        os.makedirs(self.path, exist_ok=True)
 
     def new(self, name=''):
-        old_number = get_last_folder_number(self.path)
-        new_number = old_number + 1
-
-        if len(name) > 0:
-            exp_folder = '{:02d}_{}'.format(new_number, name)
-        else: exp_folder = '{:02d}'.format(new_number)
-        os.makedirs(os.path.join(self.path, exp_folder), exist_ok=True)
-        return exp_folder
-
-    def subExperiment(self, name):
         return Experiment(name, self)
 
-def main():
-    my_exp = Experiment('experiments')
-    print(my_exp.new('nasnet'))
-    print(my_exp.new('vgg'))
-    print(my_exp.new('inceptionv3'))
-    adam = my_exp.subExperiment('adam')
-    print(adam.new('001'))
-    print(adam.new('0001'))
+    def __str__(self):
+        return self.path
 
-if __name__ == '__main__':
-    main()
+    def __repr__(self):
+        return 'Experiment({}, {})'.format(self.name, self.parent)
