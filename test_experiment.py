8d62707a2b47c4e3bcfdf8eb9f138b773339c6fd
Saravanabalagi R <saravanabalagi@hotmail.com>
2019-04-28 13:58:21 +0100

    Refactoring

--- test_experiment.py
+++ test_experiment.py
@@ -2,7 +2,7 @@ from experiment import Experiment
 import shutil
 import os
 
-def new_helper(name, number, parent):
+def experiment_new_helper(name, number, parent):
     test_sub_experiment = parent.new(name)
     expected_path = os.path.join(parent.path, '{}_{}'.format(number, name))
     assert (test_sub_experiment.path == expected_path) is True
@@ -12,20 +12,20 @@ def new_helper(name, number, parent):
     assert (test_sub_experiment.path == expected_path) is True
     assert (test_sub_experiment.parent == parent) is True
 
-def test_experment_new():
+def test_experiment_new():
 
     EXPERIMENT_ROOT = 'experiments';
     shutil.rmtree(EXPERIMENT_ROOT, ignore_errors=True)
 
     test_experiment = Experiment(EXPERIMENT_ROOT)
 
-    new_helper('nasnet', '00', test_experiment)
-    new_helper('vgg_16', '01', test_experiment)
-    new_helper('inception_v3', '02', test_experiment)
+    experiment_new_helper('nasnet', '00', test_experiment)
+    experiment_new_helper('vgg_16', '01', test_experiment)
+    experiment_new_helper('inception_v3', '02', test_experiment)
 
     test_sub_experiment = test_experiment.new('adam')
 
-    new_helper('001', '00', test_sub_experiment)
-    new_helper('0001', '01', test_sub_experiment)
+    experiment_new_helper('001', '00', test_sub_experiment)
+    experiment_new_helper('0001', '01', test_sub_experiment)
 
     shutil.rmtree(EXPERIMENT_ROOT, ignore_errors=True)
